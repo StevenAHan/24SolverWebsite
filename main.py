d37f591576fa3800e2ba9a563e637393232d0966
majikyouser <sahiagb@gmail.com>
2022-11-08 10:28:33 -0500

    Created Virtual environment

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from flask import Flask, render_template
+
 import subprocess
 
 output = subprocess.run(["./solver", "1", "5", "5", "5", "y"], stdout = subprocess.PIPE, universal_newlines = True).stdout
