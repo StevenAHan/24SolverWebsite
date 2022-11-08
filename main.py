d729d4c1ccf3021bb2d3ea5b5d75a178f627a063
majikyouser <sahiagb@gmail.com>
2022-11-08 10:25:07 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import subprocess
+
+output = subprocess.run(["./solver", "1", "5", "5", "5", "y"], stdout = subprocess.PIPE, universal_newlines = True).stdout
+output = output[:-1] # remove endline at the end of output
+print(output)
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int yes;
+    cin >> yes;
+    cout << yes << "Hello World" << endl;
+}
